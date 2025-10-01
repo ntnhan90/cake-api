6b51ad53af7d2f0b6c6644839ae02ae943716bb0
nguyễn thái nhân <ntnhan90@gmail.com>
2025-10-01 22:58:16 +0700

    auth, remove ecommerce module

---
+++ src/modules/auth/dto/login.res.dto.ts
@@ -0,0 +1,22 @@
+import { NumberField, StringField } from "@/decorators/field.decorators";
+import { Exclude, Expose } from "class-transformer";
+
+@Exclude()
+export class LoginResDto {
+    @Expose()
+    @StringField()
+    userId!: string;
+
+    @Expose()
+    @StringField()
+    accessToken!: string;
+
+    @Expose()
+    @StringField()
+    refreshToken!: string;
+
+    @Expose()
+    @NumberField()
+    tokenExpires!: number;
+
+}
--- src/modules/ecommerce/dto/update-ecommerce.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEcommerceDto } from './create-ecommerce.dto';
-
-export class UpdateEcommerceDto extends PartialType(CreateEcommerceDto) {}
